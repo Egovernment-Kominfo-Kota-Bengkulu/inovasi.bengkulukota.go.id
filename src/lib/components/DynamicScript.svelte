44d7f1b3e929f21f2f596634f8cca8ea1e25e669
Iit Pranata <iitpranataip@gmail.com>
2025-05-16 14:07:22 +0700

    first commit

---
+++ src/lib/components/DynamicScript.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    export let script = [
+        '/assets/libs/jquery/jquery.min.js',
+        '/assets/libs/bootstrap/js/bootstrap.bundle.min.js',
+        '/assets/libs/metismenu/metisMenu.min.js',
+        '/assets/libs/simplebar/simplebar.min.js',
+        '/assets/libs/node-waves/waves.min.js',
+        '/assets/libs/feather-icons/feather.min.js',
+        '/assets/libs/pace-js/pace.min.js',
+        '/assets/js/app.js'
+  ];
+
+  onMount (() => {
+    script.forEach(src => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    });
+  })
+</script>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import DynamicScript from "$lib/components/DynamicScript.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import Header from "$lib/components/Header.svelte";
+  import RightBar from "$lib/components/RightBar.svelte";
+  import VerticalMenu from "$lib/components/VerticalMenu.svelte";
+  let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>Starter Page | Minia - Minimal Admin & Dashboard Template</title>
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    
+    <!-- preloader css -->
+    <link rel="stylesheet" href="assets/css/preloader.min.css" type="text/css" />
+    <!-- Bootstrap Css -->
+    <link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+</svelte:head>
+
+<div id="layout-wrapper">
+
+    <Header />
+    <VerticalMenu />
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+        <div class="page-content">
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <main>
+                    {@render children()}
+                </main>
+                <!-- end page title -->
+                
+            </div> <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+
+    </div>
+    <!-- end main content-->
+    <Footer />
+
+</div>
+<!-- END layout-wrapper -->
+
+<RightBar />
+
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+<DynamicScript />
+
