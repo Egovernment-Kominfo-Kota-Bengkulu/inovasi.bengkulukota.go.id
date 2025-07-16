53bc63cfbf382ad54cecaa29b7e5475c60b57fcd
Iit Pranata <iitpranataip@gmail.com>
2025-07-16 16:15:59 +0700

    fix(index): kembali lagi ke side

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
    const api = axios.create({
-     baseURL: import.meta.env.PUBLIC_API_URL,
+     baseURL: 'https://inovasibackend.bengkulukota.go.id/api',
      headers: {
        'Content-Type': 'application/json',
      },
